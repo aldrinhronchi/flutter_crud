9428cb8f075ee8031b811e5347c6eb5156e8785d
Terore <47401383+aldrinhronchi@users.noreply.github.com>
2020-12-18 19:56:02 -0300

    Primeiro video feito

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -3,8 +3,14 @@ import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
 import 'package:provider/provider.dart';
 
-class UserForm extends StatelessWidget {
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
   final _form = GlobalKey<FormState>();
+
   final Map<String, String> _formData = {};
 
   void _loadFormData(User user) {
@@ -17,11 +23,13 @@ class UserForm extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void didChangeDependencies() {
+    super.didChangeDependencies();
     final User user = ModalRoute.of(context).settings.arguments;
-
     _loadFormData(user);
+  }
 
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulario de Uusuario'),
@@ -67,6 +75,7 @@ class UserForm extends StatelessWidget {
                 onSaved: (value) => _formData['name'] = value,
               ),
               TextFormField(
+                keyboardType: TextInputType.emailAddress,
                 initialValue: _formData['email'],
                 decoration: InputDecoration(labelText: 'E-mail'),
                 onSaved: (value) => _formData['email'] = value,
